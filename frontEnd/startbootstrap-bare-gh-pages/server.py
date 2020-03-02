6a7ae0610d26de270e25bdb774d01863551a3e9a
Jason <zhang98@bu.edu>
2020-03-01 22:58:50 -0500

    api working and server can host locally, still need html5 geolocation and plotting merchants

--- frontEnd/startbootstrap-bare-gh-pages/server.py
+++ frontEnd/startbootstrap-bare-gh-pages/server.py
@@ -3,26 +3,54 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/search', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def yelpCall():
-    zipCode = request.form['city']
-    url = 'https://api.yelp.com/v3/businesses/search'
-    headers = {'Authorization': 'Bearer'}
-    params = {'location': 'San Bruno',
-          'term': 'Japanese Restaurant',
-          'pricing_filter': '1, 2',
-          'sort_by': 'rating'
-         }
+    # information passed in from the form
+    type = request.form['type']
+    city = request.form['city']
+    price = request.form['price']
 
+    # api-endpoint
+    url = 'https://api.yelp.com/v3/businesses/search'
+
+    # authorization using access token
+    headers = {'Authorization': 'Bearer'}
+
+    # parameters I'm passing in to the api request
+    params = {'term': type,
+              'location': city,
+              'price': price,
+              'limit': 2
+              }
+    # api-call
     r = requests.get(url=url, params=params, headers=headers)
+
+    # information passed back from api
     apiDict = r.json()
-    businesses = apiDict['businesses']
-    return str(businesses)
-    # return render_template('results.html')
+
+    # list of dictionaries, each index is an individual business
+    businessList = apiDict['businesses']
+
+    # information I want
+    nameList = []
+    photoURLS = []
+    displayAddresses = []
+    for business in businessList:
+        nameList.append(business['name'])
+
+        # display addresses holds a list of lists, the first index tells you the restaurant and the second index is
+        # the address split by commas
+        displayAddresses.append(business['location']['display_address'])
+        # for image in business['photos']:
+            # photoURLS.append(image)
+    # return str(businessList)
+    # return str(str(nameList) + '\n' + str(displayAddresses))
+    return render_template('results.html', name=nameList[0], address=displayAddresses[0])
 
 @app.route('/')
 def main():
     return render_template('mainpage.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
