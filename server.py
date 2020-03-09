493b9b3ef2359b0b8b0d9ca15851ba126674ecff
jzhang0107 <zhang98@bu.edu>
2020-03-08 23:06:21 -0400

    praying

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('helloWorld.html')
+    return render_template('index.html')
 
 
 @app.route('/initialResults', methods=['POST'])
